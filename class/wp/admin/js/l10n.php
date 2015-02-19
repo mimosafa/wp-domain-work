0cee25cca96f3b29e8e00f41298ebbfd7ff81e08
mimoto.t <mimoto@w-tokyodo.com>
2015-02-19 19:33:14 +0900

    Change post status classes
    add trait built_in

--- class/wp/admin/js/l10n.php
+++ class/wp/admin/js/l10n.php
@@ -2,43 +2,95 @@
 
 namespace WP_Domain_Work\WP\admin\js;
 
+/**
+ * L10n data
+ *
+ * commonL10n
+ * - handle: common
+ *
+ * quicktagsL10n
+ * - handle: quicktags
+ *
+ * wpPointerL10n
+ * - handle: wp-pointer
+ *
+ */
 class L10n {
 	use \WP_Domain_Work\Utility\Singleton;
 
 	/**
-	 * @var arrat
-	 * @see https://github.com/WordPress/WordPress/blob/4.1-branch/wp-includes/script-loader.php#L441
+	 * @var array
 	 */
-	private static $postL10n = [];
+	private static $data = [
+		/**
+		 * @see https://github.com/WordPress/WordPress/blob/4.1-branch/wp-includes/script-loader.php#L441
+		 */
+		'postL10n' => [],
+	];
+
+	/**
+	 * @var string
+	 */
+	private $script_tag_id;
+
+	/**
+	 * @var string
+	 */
+	private static $scripts = '';
 
 	protected function __construct() {
+		$this->script_tag_id = strtolower( str_replace( [ '_', '\\' ], '-', __CLASS__ ) );
 		$this->init();
 	}
 
 	private function init() {
-		add_action( 'admin_footer-post.php', [ $this, 'js_postL10n' ], 99 );
-		add_action( 'admin_footer-post-new.php', [ $this, 'js_postL10n' ], 99 );
+		add_action( 'admin_footer', [ $this, 'init_scripts' ] );
 	}
 
+	/**
+	 * @access public
+	 *
+	 * @param  string $context postL10n|..
+	 * @param  string $key
+	 * @param  string $text
+	 * @return bool
+	 */
 	public static function set( $context, $key, $text ) {
 		if ( ! is_string( $key ) || ! $key ) {
 			return false;
 		}
 		$_ = self::getInstance();
-		if ( ! property_exists( $_, $context ) ) {
+		if ( ! array_key_exists( $context, $_::$data ) ) {
 			return false;
 		}
-		$_::${$context}[$key] = wp_json_encode( $text );
+		$_::$data[$context][$key] = wp_json_encode( $text );
 	}
 
-	public function js_postL10n() {
-		if ( ! self::$postL10n ) {
-			return;
+	public function init_scripts() {
+		foreach ( self::$data as $context => $texts ) {
+			if ( $texts ) {
+				self::$scripts .= "  window.{$context} = window.{$context} || {};\n";
+				foreach ( $texts as $before => $after ) {
+					self::$scripts .= "  {$context}.{$before} = {$after};\n";
+				}
+			}
 		}
-		echo "<script type='text/javascript'>\n";
-		foreach ( self::$postL10n as $before => $after ) {
-			echo "  postL10n.{$before} = {$after};\n";
+		if ( self::$scripts ) {
+			# add_action( 'admin_footer-edit.php', [ $this, 'print_script_tag' ], 99 );
+			if ( self::$data['postL10n'] ) {
+				add_action( 'admin_footer-post.php',     [ $this, 'print_script_tag' ], 99 );
+				add_action( 'admin_footer-post-new.php', [ $this, 'print_script_tag' ], 99 );
+			}
 		}
-		echo "</script>\n";
 	}
+
+	public function print_script_tag() {
+		$tag_id  = esc_attr( $this->script_tag_id );
+		$scripts = self::$scripts;
+		echo <<<EOF
+<script type='text/javascript' id='{$tag_id}'>
+{$scripts}</script>\n
+EOF;
+	}
+
 }
--- class/wp/post/post-status/pending.php
+++ class/wp/post/post-status/pending.php
@@ -45,96 +45,22 @@ namespace WP_Domain_Work\WP\post\post_status;
  * @link https://github.com/WordPress/WordPress/blob/4.1-branch/wp-includes/script-loader.php#L454
  */
 class pending {
+	use built_in;
 
-	private $labels = [
+	protected $labels = [
 		'label'       => '',
 		'description' => '',
 		'save_action' => '',
 		'states'      => '',
 	];
 
-	private $texts = [];
-
-	private $texts_with_contexts = [];
-
-	private static $defaults = [
+	protected static $defaults = [
 		'label'       => [ 'Pending', 'post' ],
 		'description' => 'Pending Review',
 		'save_action' => 'Save as Pending',
 		'states'      => [ 'Pending', 'post state' ],
 	];
 
-	public function __construct( Array $args ) {
-		if ( ! $this->set_texts( $args ) ) {
-			return;
-		}
-		$this->init();
-	}
-
-	protected function set_texts( Array $args ) {
-		if ( ! array_key_exists( 'label', $args ) || ! is_string( $args['label'] ) || ! $args['label'] ) {
-			return false;
-		}
-		array_walk( $this->labels, function( &$string, $key, $args ) {
-			if ( array_key_exists( $key, $args ) && is_string( $args[$key] ) && $args[$key] ) {
-				$string = $args[$key];
-			} else {
-				$string = $args['label'];
-			}
-		}, $args );
-		foreach ( self::$defaults as $key => $string ) {
-			if ( is_array( $string ) ) {
-				$text_context = implode( '__', $string );
-				$this->texts_with_contexts[$text_context] = $this->labels[$key];
-			} else {
-				$this->texts[$string] = $this->labels[$key];
-			}
-		}
-		return true;
-	}
-
-	protected function init() {
-		$this->register_post_status();
-		if ( is_admin() ) {
-			$this->gettext();
-			$this->gettext_with_context();
-			$this->js_texts();
-		}
-	}
-
-	protected function register_post_status() {
-		global $wp_post_statuses;
-		$statusObj = $wp_post_statuses['pending'];
-		$statusObj->label = $this->labels['label'];
-		if ( is_admin() ) {
-			$label_count_string = sprintf( '%s <span class="count">(%%s)</span>', $this->labels['label'] );
-			$statusObj->label_count = _n_noop( $label_count_string, $label_count_string );
-		}
-	}
-
-	protected function gettext() {
-		add_filter( 'gettext', function( $translated, $text ) {
-			if ( array_key_exists( $text, $this->texts ) ) {
-				$translated = $this->texts[$text];
-			}
-			return $translated;
-		}, 10, 2 );
-	}
-
-	protected function gettext_with_context() {
-		add_filter( 'gettext_with_context', function( $translated, $text, $context ) {
-			$text_context = $text . '__' . $context;
-			if ( array_key_exists( $text_context, $this->texts_with_contexts ) ) {
-				$translated = $this->texts_with_contexts[$text_context];
-			}
-			return $translated;
-		}, 10, 3 );
-	}
-
-	/**
-	 * gettext へのフィルター処理が、postL10n のローカライズ('wp_default_scripts')に間に合わないため、やむを得ず js で上書き
-	 * @see https://github.com/WordPress/WordPress/blob/4.1-branch/wp-includes/script-loader.php#L1041
-	 */
 	protected function js_texts() {
 		\WP_Domain_Work\WP\admin\js\L10n::set( 'postL10n', 'savePending', $this->labels['save_action'] );
 	}
