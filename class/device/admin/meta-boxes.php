34e40eacd0e4ff69abeb5f30723ab407559176a3
mimoto.t <mimoto@w-tokyodo.com>
2015-06-02 18:56:58 +0900

    Dev in Higashiyaguchi

--- class/device/admin/meta-boxes.php
+++ class/device/admin/meta-boxes.php
@@ -42,7 +42,7 @@ class meta_boxes extends post {
 		parent::__construct( $domain );
 		$this->box_id_prefix = parent::BOX_ID_PREFIX . $domain . '-';
 		self::$_defaults['callback'] = [ &$this, 'meta_box' ];
-		add_action( 'add_meta_boxes', [ &$this, 'add_meta_boxes' ] );
+		add_action( 'add_meta_boxes', [ &$this, 'add_meta_boxes' ], 10, 2 );
 	}
 
 	/**
@@ -64,14 +64,13 @@ class meta_boxes extends post {
 	public function add( Array $args ) {
 		if ( ! $args = $this->prepare_arguments( $args ) )
 			return;
+		$this->meta_boxes[] = array_merge( self::$_defaults, $args );
+		/*
 		$args = array_merge( self::$_defaults, $args );
-		if ( ! $args['id'] )
-			$args['id'] = implode( '-', (array) $args['asset'] );
-		if ( ! $args['title'] )
-			$args['title'] = ucwords( str_replace( [ '-', '_' ], [ ' / ', ' ' ], $args['id'] ) );
 		$callback_args = array_splice( $args, 6 );
 		extract( $args );
-		$this->meta_boxes[] = [ $this->box_id_prefix . $id, $title, $callback, $screen, $context, $priority, $callback_args ];
+		$this->meta_boxes[] = [ $this->box_id_prefix . $id, $title, $callback, null, $context, $priority, $callback_args ];
+		*/
 	}
 
 	/**
@@ -103,38 +102,41 @@ class meta_boxes extends post {
 
 	/**
 	 * @access public
+	 *
+	 * @param  string  $post_type
+	 * @param  WP_Post $post
 	 */
-	public function add_meta_boxes() {
+	public function add_meta_boxes( $post_type, \WP_Post $post ) {
 		if ( $this->meta_boxes ) {
-			$data = [];
 			foreach ( $this->meta_boxes as $args ) {
 				/**
-				 * @var array $args {
-				 *     @see  http://codex.wordpress.org/Function_Reference/add_meta_box
-				 *
-				 *     @type string   $id
-				 *     @type string   $title
-				 *     @type callable $callback
-				 *     @type string   $screen
-				 *     @type string   $context
-				 *     @type string   $priority
-				 *     @type array    $callback_args {
-				 *
-				 *         @type string|array $asset Callable string ( OR strings array )
-				 *                                   by WP_Domain\{$domain}\property::get()
-				 *
-				 *         @type string       $description Optional.
-				 *
-				 *         ...and so on
-				 *     }
-				 * }
+				 * @var string $id
+				 * @var string $title
+				 * @var array|string $asset
+				 * @var string $description
 				 */
-				call_user_func_array( 'add_meta_box', $args );
+				extract( $args );
 
-				$data[] = $args[0];
+				foreach ( (array) $asset as $a ) {
+					self::$asset_values[$a] = $this->property->$a->get( $post );
+				}
+
+				$callback_args = [ 'asset' => $asset ];
+				if ( isset( $description ) )
+					$callback_args['description'] = $description;
+
+				//$cb = [ &$this, 'print_fieldset_' . $asset ];
+
+				add_meta_box( $this->box_id_prefix . $id, $title, $callback, $post_type, $context, $priority, $callback_args );
 			}
-			\WPDW\Scripts::add_data( 'metaboxes', $data );
 		}
+	}
+
+	public function __call( $func, $a ) {
+		if ( preg_match( '/\Aprint_fieldset_([a-z][a-z0-9_]+)/', $func, $m ) ) :
+			$asset = $m[1];
+			printf( '<fieldset id="%s"></fieldset>', esc_attr( self::FORM_ID_PREFIX . $asset ) );
+		endif;
 	}
 
 	/**
--- class/device/admin/post.php
+++ class/device/admin/post.php
@@ -7,7 +7,6 @@ namespace WPDW\Device\Admin;
  * - WPDW\Device\Admin\edit_form_advamced
  */
 abstract class post {
-	use \WPDW\Util\Array_Function;
 
 	const FORM_ID_PREFIX = 'wpdw-form-';
 	const BOX_ID_PREFIX  = 'wpdw-meta-box-';
@@ -27,6 +26,8 @@ abstract class post {
 	 * @var array
 	 */
 	protected static $done_assets = [];
+	protected static $admin_forms = [];
+	protected static $asset_values = [];
 
 	/**
 	 * Constructor
@@ -41,15 +42,14 @@ abstract class post {
 	public function __construct( $domain ) {
 		if ( ! $domain = filter_var( $domain ) )
 			return;
+
 		$this->property = \WPDW\_property( $domain );
+
 		if ( ! self::$template )
 			self::$template = new template( $domain );
 
 		static $done = false;
 		if ( ! $done ) {
-			/**
-			 * Exclude wrote custom fields from post custom meta box
-			 */
 			add_filter( 'is_protected_meta', [ $this, 'is_protected_meta' ], 10, 3 );
 			add_action( 'admin_enqueue_scripts', [ &$this, 'localize_form_data' ], 8 );
 			$done = true;
@@ -66,30 +66,27 @@ abstract class post {
 	 */
 	protected function prepare_arguments( Array $args ) {
 		$args = filter_var_array( $args, $this->get_filter_definition() );
-		if ( $args['asset'] ) {
-			/**
-			 * Set title if not defined
-			 */
-			if ( ! $args['title'] ) {
-				$assets = is_array( $args['asset'] ) ? array_filter( self::flatten( $args['asset'], true ) ) : (array) $args['asset'];
-				$args['title'] = implode(
-					' / ',
-					array_map( function( $asset ) {
-						return $this->property->get_setting( $asset )['label'];
-					}, $assets )
-				);
-				$args['asset'] = count( $assets ) > 1 ? $assets : array_shift( $assets );
-			}
+		$args['asset'] = \WPDW\Util\Array_Function::flatten( (array) $args['asset'], true );
+		if ( ! $args['asset'] )
+			return;
 
-			//$setting = $this->property->get_setting( $args['asset'] );
-		}
-		$args = array_filter( $args );
-		if ( array_key_exists( 'asset', $args ) )
-			return $args;
-		else if ( array_key_exists( 'callback', $args ) && array_key_exists( 'id', $args ) )
-			return $args;
-		else
-			return null;
+		$id = '';
+		$ttl = $args['title'] ? null : '';
+		$assetWalker = function( $asset ) use ( &$id, &$ttl ) {
+			self::$admin_forms[] = $asset;
+			$id .= $asset . '-';
+			if ( is_string( $ttl ) )
+				$ttl .= $this->property->get_setting( $asset )['label'] . ' / ';
+		};
+		array_walk( $args['asset'], $assetWalker );
+
+		$args['id'] = rtrim( $id, '-' );
+		if ( $ttl )
+			$args['title'] = rtrim( $ttl, '/ ' );
+
+		$args['asset'] = count( $args['asset'] ) > 1 ? $args['asset'] : array_shift( $args['asset'] );
+
+		return array_filter( $args );
 	}
 
 	/**
@@ -122,15 +119,8 @@ abstract class post {
 				return $var;
 			};
 
-			// Callback
-			$callbackVar = function( $var ) {
-				return is_callable( $var ) ? $var : null;
-			};
-
 			$def = [
-				'id'    => [ 'filter' => \FILTER_VALIDATE_REGEXP, 'options' => [ 'regexp' => '/\A[a-z][a-z0-9_\-]+\z/', 'default' => null ] ],
 				'title' => \FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-				'callback' => [ 'filter' => \FILTER_CALLBACK, 'options' => $callbackVar ],
 				'asset'    => [ 'filter' => \FILTER_CALLBACK, 'options' => $assetVar ],
 				'description' => \FILTER_SANITIZE_FULL_SPECIAL_CHARS,
 			];
@@ -183,12 +173,14 @@ abstract class post {
 	}
 
 	public function localize_form_data() {
-		if ( self::$done_assets ) {
+		if ( self::$admin_forms && self::$done_assets ) {
 			$data = [];
 			foreach ( self::$done_assets as $asset ) {
 				$data[$asset] = $this->property->$asset->get_recipe();
 			}
-			\WPDW\Scripts::add_data( 'forms', $data );
+			\WPDW\Scripts::add_data( 'assets', $data );
+			\WPDW\Scripts::add_data( 'adminForms', self::$admin_forms );
+			\WPDW\Scripts::add_data( 'assetValues', self::$asset_values );
 			\WPDW\Scripts::add_data( 'form_id_prefix', self::FORM_ID_PREFIX );
 		}
 	}
